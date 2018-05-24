5c162fb71c834c6762ab5a5c2e18471f849637ac
julien boyer <juh@MacBook-Pro-de-julien.local>
2018-05-24 17:08:52 +0200

    Add data faker

--- app/src/main/java/com/example/juh/poikeeper/BaseApplication.java
+++ app/src/main/java/com/example/juh/poikeeper/BaseApplication.java
@@ -3,6 +3,7 @@ package com.example.juh.poikeeper;
 import android.app.Application;
 
 import com.activeandroid.ActiveAndroid;
+import com.example.juh.poikeeper.model.PointOfInterest;
 import com.mapbox.mapboxsdk.Mapbox;
 
 public class BaseApplication extends Application {
@@ -11,5 +12,8 @@ public class BaseApplication extends Application {
         super.onCreate();
         ActiveAndroid.initialize(this);
         Mapbox.getInstance(this, getString(R.string.token_access_mapboxgl));
+
+        if (PointOfInterest.isEmpty())
+            PointOfInterest.FakeDateInit();
     }
 }
--- app/src/main/java/com/example/juh/poikeeper/model/PointOfInterest.java
+++ app/src/main/java/com/example/juh/poikeeper/model/PointOfInterest.java
@@ -9,6 +9,7 @@ import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Select;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 
+import java.util.Date;
 import java.util.List;
 
 @Table(name = "point_of_interest")
@@ -104,5 +105,78 @@ public final class PointOfInterest extends Model {
         return (new Select()).from(PointOfInterest.class)
                 .where("Id = ?", id)
                 .executeSingle();
+    }
+
+    public static boolean isEmpty() {
+        int entries = (new Select()).from(PointOfInterest.class).count();
+        return entries <= 0;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////
+    // FAKING DATA
+    //////////////////////////////////////////////////////////////////////////////////////
+
+    public static void FakeDateInit() {
+        PointOfInterest[] pois = new PointOfInterest[4];
+        String[] titles = {
+                "Meriadeck Pôle Emplois",
+                "Meriadeck Auchan Sud",
+                "Chartron Ecole Osteo",
+                "Le Lac Stade"
+        };
+        String[] descs = {
+          "Spot mental avec de gros saut en hauteurs",
+          "Saut de bras avec une suite de saut de prec'",
+          "Escalade urbaine avec un toit hors-norme",
+          "Longue piste avec beaucoup de vault"
+        };
+        LatLng[] latLng = {
+                new LatLng(44.83819,-0.5896532),
+                new LatLng(44.83838,-0.585887),
+                new LatLng(44.85521,-0.5692514),
+                new LatLng(44.898838,-0.565737)
+        };
+
+        for (int i = 0; i < pois.length; i++) {
+            pois[i] = new PointOfInterest(titles[i], descs[i], null, latLng[i]);
+            pois[i].save();
+            fakeTask(pois[i]);
+        }
+    }
+
+    static String[] titles = {
+            "Saut de chat",
+            "Passe muraille",
+            "Saut de précision",
+            "Speed vault"
+    };
+
+    static String[] names = new String[]{
+            "Muret blanc",
+            "Mur du batiment gris",
+            "Barre pour piétons",
+            "barrière à vélos"
+    };
+
+    static String[] descs = {
+            "Prendre de l'élan à partir du banc pour effectuer le saut de chat sur le muret en face",
+            "Continuer sa course vers le mur du bar tabac gris, et effectuer un passe muraille sur 3m10",
+            "Courir vers le fond du toit et sauter en précision sur la barrière pietonne",
+            "Effectuer le dernier mouvement, un speed vault pour revenir sur le trottoir"
+    };
+
+    private static void fakeTask(PointOfInterest poi) {
+        for (int i = 0; i < titles.length; i++) {
+            PoiTask task = new PoiTask();
+
+            task.checked = false;
+            task.title = titles[i];
+            task.name = names[i];
+            task.description = descs[i];
+            task.date = new Date();
+            task.pointOfInterest = poi;
+
+            task.save();
+        }
     }
 }
